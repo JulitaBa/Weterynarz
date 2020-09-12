e35a4090f032cfd82e64b055591520785072dd45
Kuba Brodowski <kuba.brodowski@gmail.com>
2020-09-12 14:00:11 +0200

    add contact form (only backend)

--- mysite/vet/models.py
+++ mysite/vet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    email = models.CharField('Email', max_length=200)
+    subject = models.CharField('Tytuł', max_length=200)
+    message = models.TextField('Wiadomość', max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
