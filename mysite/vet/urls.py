5fa635e9b8c413a2e3247f55048eab4a56a49639
Julita <julita.babecka@gmail.com>
2020-09-19 01:27:01 +0200

    Przekopiowanie treści i stworzenie linków

--- mysite/vet/urls.py
+++ mysite/vet/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path('onas/', views.onas, name='onas'),
     path('internistyczne/', views.internistyczne, name='internistyczne'),
     path('profilaktyczne/', views.profilaktyczne, name='profilaktyczne'),
+    path('chirurgiczne/', views.chirurgiczne, name='chirurgiczne'),
+    path('ortopedyczne/', views.ortopedyczne, name='ortopedyczne'),
+    path('badania_lab/', views.badania_lab, name='badania_lab'),
+    path('rtg_ekg/', views.rtg_ekg, name='rtg_ekg'),
+    path('pielegnacja/', views.pielegnacja, name='pielegnacja'),
+    path('paszporty/', views.paszporty, name='paszporty'),
+    path('czipowanie/', views.czipowanie, name='czipowanie'),
+    path('diety/', views.diety, name='diety'),
 
 ]
--- mysite/vet/views.py
+++ mysite/vet/views.py
@@ -13,7 +13,22 @@ def internistyczne(request):
     return render(request, 'vet/internistyczne.html')
 def profilaktyczne(request):
     return render(request, 'vet/profilaktyczne.html')
-
+def chirurgiczne(request):
+    return render(request, 'vet/chirurgiczne.html')
+def ortopedyczne(request):
+    return render(request, 'vet/ortopedyczne.html')
+def badania_lab(request):
+    return render(request, 'vet/badania_lab.html')
+def rtg_ekg(request):
+    return render(request, 'vet/rtg_ekg.html')
+def pielegnacja(request):
+    return render(request, 'vet/pielegnacja.html')
+def paszporty(request):
+    return render(request, 'vet/paszporty.html')
+def czipowanie(request):
+    return render(request, 'vet/czipowanie.html')
+def diety(request):
+    return render(request, 'vet/diety.html')
 
 def sendMessage(request):
     if request.method == 'GET':
