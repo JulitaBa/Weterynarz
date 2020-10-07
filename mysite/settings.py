066f5a44a53a1245718a9ad991831c81b3f6aeeb
Julita <julita.babecka@gmail.com>
2020-10-07 14:35:59 +0200

    Contact form - disappearing message

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,6 @@ STATIC_URL = '/static/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'julita.babecka@gmail.com'
-EMAIL_HOST_PASSWORD = 'julita89'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
